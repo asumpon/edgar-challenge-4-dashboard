13a3e5742113526dd12c4f03c1bbfa2c3c3ddb93
asumpon <asumpon@smu.edu>
2020-12-06 03:25:00 -0600

    configuring for heroku

--- core/settings.py
+++ core/settings.py
@@ -7,6 +7,7 @@ import os
 from decouple import config
 from unipath import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,6 @@ STATICFILES_DIRS = (
 )
 #############################################################
 #############################################################
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
+
