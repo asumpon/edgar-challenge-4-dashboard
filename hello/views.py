36b588cad75d0a6acd6c99c2a07f3b5d179879dc
asumpon <asumpon@smu.edu>
2020-12-05 23:03:14 -0600

    added print

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 import json, plotly
 # from hello.wrangle_data import return_figures
 from .models import Greeting
-from plotly import express as px
 
 # Create your views here.
 def index(request):
@@ -13,27 +12,11 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html",ids=ids,figuresJSON=figuresJSON)
     
-    # fig =px.scatter(x=range(10), y=range(10))
-    # graph = fig.to_html(full_html=False, default_height=500, default_width=700)
-    # context = {'graph': graph}  
-  
-    gapminder = px.data.gapminder()
-
-    fig = px.scatter(
-        gapminder.query("year==2007"),
-        x="gdpPercap",
-        y="lifeExp",
-        size="pop",
-        color="continent",
-        hover_name="country",
-        log_x=True,
-        size_max=60,
-        height=400,
-        width=650,
-    )
-    with open('index.html', 'w') as f:
-        f.write(fig.to_html(include_plotlyjs='cdn'))
-    return render(request, "index.html")
+    fig =px.scatter(x=range(10), y=range(10))
+    graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+    print(graph)
+    context = {'graph': graph}  
+    return render(request, "index.html", context)
 
 
 def db(request):
