d7abdbc70f4ff5a57a8ea420dd59635794b426ab
asumpon <asumpon@smu.edu>
2020-12-05 22:03:48 -0600

    updating views

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json, plotly
+from wrangling_scripts.wrangle_data import return_figures
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
+    figures = return_figures()
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    return render(request, "index.html",ids=ids,figuresJSON=figuresJSON)
 
 
 def db(request):
