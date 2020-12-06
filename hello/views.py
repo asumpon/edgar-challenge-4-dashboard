ff7f93e279fbffb218667ec1f4fb19fd794291c3
asumpon <asumpon@smu.edu>
2020-12-05 23:00:27 -0600

    it didn't change

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json, plotly
 # from hello.wrangle_data import return_figures
-import plotly.express as px
 from .models import Greeting
+from plotly import express as px
 
 # Create your views here.
 def index(request):
@@ -13,8 +13,26 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html",ids=ids,figuresJSON=figuresJSON)
     
-    fig =px.scatter(x=range(10), y=range(10))
-    fig.write_html("index.html")
+    # fig =px.scatter(x=range(10), y=range(10))
+    # graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+    # context = {'graph': graph}  
+  
+    gapminder = px.data.gapminder()
+
+    fig = px.scatter(
+        gapminder.query("year==2007"),
+        x="gdpPercap",
+        y="lifeExp",
+        size="pop",
+        color="continent",
+        hover_name="country",
+        log_x=True,
+        size_max=60,
+        height=400,
+        width=650,
+    )
+    with open('index.html', 'w') as f:
+        f.write(fig.to_html(include_plotlyjs='cdn'))
     return render(request, "index.html")
 
 
